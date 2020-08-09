3d9e6b972a24638a1095d82e319611b3973bc66c
liuzq <liuzq@mail.taiji.com.cn>
2020-08-09 14:15:09 +0800

    11

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -11,7 +11,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    let len = nums.length
+    let result
+    for(let i = 0; i < len-1; i++) {
+        for(let j = len-1; j > i; j--) {
+            if(nums[i] + nums[j] === target) {
+                result = [i, j]
+            }
+        }
+    }
+    return result
 };
 // @lc code=end
 
--- 11.盛最多水的容器.js
+++ 11.盛最多水的容器.js
@@ -8,9 +8,25 @@
 /**
  * @param {number[]} height
  * @return {number}
+ * 思路：
+ * 双指针，第一个指针从头开始，第二个指针从尾开始，
+ * 底边长tail-head, 高为 height[head], height[tail]二者选最小的
+ * 面积计算公式：(tail-head)*Math.min(height[head], height[tail]), 
+ * 将第一次遍历值全部放入数组，筛选最面积值存起来，Math.max.apply(null, arr)
+ * 最后再筛选一遍即可得出最大值
  */
 var maxArea = function(height) {
-
+    let len = height.length
+    let result = 0
+    for(let i = 0; i < len-1; i++) {
+        let area = []
+        for(let j = len-1; j > i; j--) {
+            area.push((j-i)*Math.min(height[i], height[j]))
+        }
+        let res = Math.max.apply(null, area)
+        result = res > result ? res : result
+    }
+    return result
 };
 // @lc code=end
 
