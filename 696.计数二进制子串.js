9c827c9d8bbcebc057f2b972132a9b0646806d1c
ABoyCDog <178657263@qq.com>
2020-08-30 21:46:09 +0800

    696

--- 696.计数二进制子串.js
+++ 696.计数二进制子串.js
@@ -10,12 +10,22 @@
  * @return {number}
  */
 var countBinarySubstrings = function(s) {
+    const counts = []
     let res = 0
-    let len = s.length
-    let cur = []
-    if(len = 1) return result
-    for(let i = 0; i < len; i++) {}
-
+    let ptr = 0
+    let n = s.length
+    while (ptr < n) {
+        const c = s.charAt(ptr)
+        let count = 0
+        while (ptr < n && s.charAt(ptr) === c) {
+            ++ptr
+            ++count
+        }
+        counts.push(count)
+    }
+    for (let i = 1; i < counts.length; ++i) {
+        res += Math.min(counts[i], counts[i - 1])
+    }
     return res
 };
 // @lc code=end
