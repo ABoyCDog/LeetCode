e688cdf8e07e056447c89b9c6c4625a7cb06517a
ABoyCDog <178657263@qq.com>
2020-08-31 23:14:34 +0800

    15

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -10,7 +10,20 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-
+    let res = []
+    let hash = {}
+    for (let i = 0; i < nums.length - 2; i++) { // 每个人
+        for (let j = i + 1; j < nums.length - 1; j++) { // 依次拉上其他每个人
+        if (hash[nums[j]] !== undefined) { // 已经有合适自己的两人组
+            res.push([nums[j]].concat(hash[nums[j]]))
+            hash[nums[j]] = undefined
+        } else { // 没有合适自己的两人组
+            let mark = 0 - nums[i] - nums[j]
+            hash[mark] = [nums[i], nums[j]]
+        }
+        }
+    }
+    return res
 };
 // @lc code=end
 
