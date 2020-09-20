2720a2a967bf9bc4be892bb70f658c550ef4fd09
ABoyCDog <178657263@qq.com>
2020-09-20 22:09:21 +0800

    +90

--- 90.子集-ii.js
+++ 90.子集-ii.js
@@ -10,7 +10,28 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
+    let res = [[]]
+    let n = nums.length
+    nums.sort()
+    const hashMap = {}
 
+    function backtrack(cur, start) {
+        if(cur.length === n) return
+       
+        for(let i = start; i < n; i++) {
+            cur.push(nums[i])
+            if(hashMap[cur] === undefined) {
+                res.push([...cur])
+                hashMap[cur] = true
+            }
+            backtrack(cur,i+1)
+            cur.pop()
+        }
+    }
+
+    backtrack([],0)
+    return res
 };
+
 // @lc code=end
 
